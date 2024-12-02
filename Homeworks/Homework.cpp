e17a3418eef04f74a9165c634bdf4c47a2f7a421
sidahmed <sidahmed@gmail.com>
2024-12-02 16:13:02 +0000

    inputs homework

--- Homeworks/Homework.cpp
+++ Homeworks/Homework.cpp
@@ -6,12 +6,35 @@ int main(){
     
     ///Ex1///
 
-    string Name = "Sidahmed Mohamed", City = "NKTT", Country = "Mauritanie";
-    int Age = 23;
-    float Monthly_Salary = 5000;
+    string Name;
+    string City ; 
+    string Country ;
+    int Age ;
+    float Monthly_Salary ;
     float Yearly_Salary = Monthly_Salary * 12;
-    char Gender = 'M';
+    char Gender ;
     bool Married = true ;
+
+    cout << "Please Enter your name "<< endl;
+    cin >> Name;
+
+    cout << "Please Enter your age "<< endl;
+    cin >> Age;
+
+    cout << "Please Enter your city" << endl;
+    cin >> City;
+
+    cout << "Please enter your Contry"<< endl;
+    cin >> Country;
+
+    cout << "Please enter your Monthly salary"<< endl;
+    cin >> Monthly_Salary;
+
+    cout << "Please enter your Gender "<< endl;
+    cin >> Gender;
+
+    cout << "Are you Married ?"<< endl;
+    cin >> Married;
 
     cout << "**********************************" << endl;
     cout <<"Name = "<< Name << endl;
@@ -26,21 +49,31 @@ int main(){
 
     ///Ex2///
 
-    int Number = 20;
-    int Number_1 = 30; 
-    int Number_2 = 10; 
-    int Summe = Number + Number_1 + Number_2;
+    int Number;
+    int Number_1 ; 
+    int Number_2 ; 
+    
+    cout << "Enter the first number"<< endl;
+    cin >> Number;
+
+    cout << "Enter the second number "<< endl;
+    cin >> Number_1;
+
+    cout << "Enter the third number"<< endl;
+    cin >> Number_2;
 
     cout << Number << " + " << endl;
     cout << Number_1 << " + " << endl;
     cout << Number_2  << endl;
     cout << "_________________________" << endl;
-    cout << "Total = " << Summe << endl << endl ;
+    cout << "Total = " << Number + Number_1 + Number_2 << endl << endl ;
 
 
     /// Ex3 ///
 
-    int age = 25;
+    int age ;
+    cout << "Enter your age" << endl;
+    cin >> age ;
 
     cout << "After 5 Years you will be "<< age + 5 << " years old.\n\n";
 
