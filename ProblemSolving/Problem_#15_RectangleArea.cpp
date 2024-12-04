90e98d42789a739bbed40dd8ece0ed7dcd334fba
sidahmed <sidahmed@gmail.com>
2024-12-04 13:27:22 +0000

    Rectangle Area

---
+++ ProblemSolving/Problem_#15_RectangleArea.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main(){
+
+
+    short int A;
+    short int B;
+    short int Area;
+
+
+
+    cout << "Enter A" << endl;
+    cin >> A;
+
+    cout << "Enter B" << endl;
+    cin >> B;
+
+    cout << endl;
+
+    Area = A * B ;
+
+    cout << "Rectangle Area = " << Area << endl;
+
+
+
+
+    return 0;
+}
