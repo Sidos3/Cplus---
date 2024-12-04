fd92d63803aa3e76e078c09a1d9db4921964ccb9
sidahmed <sidahmed@gmail.com>
2024-12-04 18:22:03 +0000

    Power of 2,3,4

---
+++ ProblemSolving/Problem_#31_Power_of_2,3,4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+
+
+    short int Number;
+
+
+
+    cout << "Enter A Number " << endl;
+    cin >> Number;
+
+    cout << endl;
+
+
+
+    cout << Number * Number << endl;
+    cout << Number * Number * Number << endl;
+    cout << Number * Number * Number * Number << endl;
+
+
+
+
+    return 0;
+}
