d4d8b200dbd788e61cbfc4b25676799b19c45560
sidahmed <sidahmed@gmail.com>
2024-12-04 15:20:48 +0000

    CircleArea_Along_The_Circumference

---
+++ ProblemSolving/Problem_#21_CircleArea_Along_The_Circumference.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+
+
+    const float PI = 3.14;
+    float L;
+    float Area;
+
+
+
+
+    cout << "Enter L" << endl;
+    cin >> L;
+
+    cout << endl;
+
+    Area = (L*L) / (4 * PI);
+
+    cout << "Circle Area Through diametre  = " << Area << endl;
+
+
+
+    return 0;
+}
