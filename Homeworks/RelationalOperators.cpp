cb3ea557a20b67762fd51af9583cb5bf870bf2a1
sidahmed <sidahmed@gmail.com>
2024-12-07 18:17:09 +0000

    Relation Operator

---
+++ Homeworks/RelationalOperators.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+int main(){
+
+    short int A, B;
+
+
+
+
+    cout << "Please Enter The First Number" << endl;
+    cin >> A;
+
+    cout << "Please Enter The Second Number" << endl;
+    cin >> B;
+
+    cout << endl;
+
+    cout << A << " = " << B << " is " << (A == B) << endl;
+    cout << A << " != " << B << " is " << (A != B) << endl;
+    cout << A << " > " << B << " is " << (A > B) << endl;
+    cout << A << " < " << B << " is " << (A < B) << endl;
+    cout << A << " >= " << B << " is " << (A >= B) << endl;
+    cout << A << " <= " << B << " is " << (A <= B) <<  endl;
+
+
+
+
+    return 0;
+}
