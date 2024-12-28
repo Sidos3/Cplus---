15dd2814ea8361c23d0dc68a0134ec8d352bc076
sidahmed <sidahmed@gmail.com>
2024-12-28 15:20:23 +0000

    Rusability home work

--- Homeworks/Resability.cpp
+++ Homeworks/Resability.cpp
@@ -48,3 +48,99 @@ int main(){
 
     return 0;
 }
+
+
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+
+// struct MyInfo
+// {
+//     string Phone;
+//     string Email;
+// };
+
+// struct MyAdress
+// {
+//     string Kartye;
+// };
+
+// struct MyData
+// {
+//     string Name;
+//     string City;
+//     string Country;
+//     short int Age;
+//     float Monthly_Salary;
+//     float Yearly_Salary = Monthly_Salary * 12;
+//     char Gender;
+//     bool Married ;
+//     MyAdress Adress;
+//     MyInfo Info;
+// };
+
+// void ReadData(MyData &Data)
+// {
+//     cout << "Please Enter your name " << endl;
+//     cin >> Data.Name;
+
+//     cout << "Please Enter your age " << endl;
+//     cin >> Data.Age;
+
+//     cout << "Please Enter your city" << endl;
+//     cin >> Data.City;
+
+//     cout << "Please enter your Contry" << endl;
+//     cin >> Data.Country;
+
+//     cout << "Please enter your Monthly salary" << endl;
+//     cin >> Data.Monthly_Salary;
+
+//     cout << "Please enter your Gender " << endl;
+//     cin >> Data.Gender;
+
+//     cout << "Are you Married ?" << endl;
+//     cin >> Data.Married;
+
+//     cout << "Please enter your Adress (Kartiye)" << endl;
+//     cin >> Data.Adress.Kartye;
+
+//     cout << "Please enter your phone number" << endl;
+//     cin >> Data.Info.Phone;
+
+//     cout << "Please enter your email" << endl;
+//     cin >> Data.Info.Email;
+// }
+
+// void PrintData(MyData Data)
+// {
+//     cout << "**********************************" << endl;
+//     cout << "Name = " << Data.Name << endl;
+//     cout << "Age = " << Data.Age  << endl;
+//     cout << "Country = " << Data.Country << endl;
+//     cout << "Monthly Salary = " << Data.Monthly_Salary << endl;
+//     cout << "Yearly Salary = " << Data.Yearly_Salary << endl;
+//     cout << "Gender = " << Data.Gender << endl;
+//     cout << "Married = " << Data.Married << endl;
+//     cout << "Lkartiye = " << Data.Adress.Kartye << endl;
+//     cout << "Phone Number = " << Data.Info.Phone << endl;
+//     cout << "Email = " << Data.Info.Email << endl;
+//     cout << "**********************************" << endl << endl;
+// }
+
+
+// int main()
+// {
+
+//     MyData MyData1;
+//     ReadData(MyData1);
+//     PrintData(MyData1);
+
+
+
+//     return 0;
+// }
+
+
--- Homeworks/Structures.cpp
+++ Homeworks/Structures.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -27,56 +28,62 @@ struct MyData
     MyInfo Info;
 };
 
+void ReadData(MyData &Data)
+{
+    cout << "Please Enter your name " << endl;
+    cin >> Data.Name;
+
+    cout << "Please Enter your age " << endl;
+    cin >> Data.Age;
+
+    cout << "Please Enter your city" << endl;
+    cin >> Data.City;
+
+    cout << "Please enter your Contry" << endl;
+    cin >> Data.Country;
+
+    cout << "Please enter your Monthly salary" << endl;
+    cin >> Data.Monthly_Salary;
+
+    cout << "Please enter your Gender " << endl;
+    cin >> Data.Gender;
+
+    cout << "Are you Married ?" << endl;
+    cin >> Data.Married;
+
+    cout << "Please enter your Adress (Kartiye)" << endl;
+    cin >> Data.Adress.Kartye;
+
+    cout << "Please enter your phone number" << endl;
+    cin >> Data.Info.Phone;
+
+    cout << "Please enter your email" << endl;
+    cin >> Data.Info.Email;
+}
+
+void PrintData(MyData Data)
+{
+    cout << "**********************************" << endl;
+    cout << "Name = " << Data.Name << endl;
+    cout << "Age = " << Data.Age  << endl;
+    cout << "Country = " << Data.Country << endl;
+    cout << "Monthly Salary = " << Data.Monthly_Salary << endl;
+    cout << "Yearly Salary = " << Data.Yearly_Salary << endl;
+    cout << "Gender = " << Data.Gender << endl;
+    cout << "Married = " << Data.Married << endl;
+    cout << "Lkartiye = " << Data.Adress.Kartye << endl;
+    cout << "Phone Number = " << Data.Info.Phone << endl;
+    cout << "Email = " << Data.Info.Email << endl;
+    cout << "**********************************" << endl << endl;
+}
+
 
 int main()
 {
+
     MyData MyData1;
-
-
-    cout << "Please Enter your name " << endl;
-    cin >> MyData1.Name;
-
-    cout << "Please Enter your age " << endl;
-    cin >> MyData1.Age;
-
-    cout << "Please Enter your city" << endl;
-    cin >> MyData1.City;
-
-    cout << "Please enter your Contry" << endl;
-    cin >> MyData1.Country;
-
-    cout << "Please enter your Monthly salary" << endl;
-    cin >> MyData1.Monthly_Salary;
-
-    cout << "Please enter your Gender " << endl;
-    cin >> MyData1.Gender;
-
-    cout << "Are you Married ?" << endl;
-    cin >> MyData1.Married;
-
-    cout << "Please enter your Adress (Kartiye)" << endl;
-    cin >> MyData1.Adress.Kartye;
-
-    cout << "Please enter your phone number" << endl;
-    cin >> MyData1.Info.Phone;
-
-    cout << "Please enter your email" << endl;
-    cin >> MyData1.Info.Email;
-
-    cout << endl;
-
-    cout << "**********************************" << endl;
-    cout << "Name = " << MyData1.Name << endl;
-    cout << "Age = " << MyData1.Age  << endl;
-    cout << "Country = " << MyData1.Country << endl;
-    cout << "Monthly Salary = " << MyData1.Monthly_Salary << endl;
-    cout << "Yearly Salary = " << MyData1.Yearly_Salary << endl;
-    cout << "Gender = " << MyData1.Gender << endl;
-    cout << "Married = " << MyData1.Married << endl;
-    cout << "Lkartiye = " << MyData1.Adress.Kartye << endl;
-    cout << "Phone Number = " << MyData1.Info.Phone << endl;
-    cout << "Email = " << MyData1.Info.Email << endl;
-    cout << "**********************************" << endl << endl;
+    ReadData(MyData1);
+    PrintData(MyData1);
 
 
 
